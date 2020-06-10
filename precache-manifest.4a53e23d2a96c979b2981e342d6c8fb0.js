18d5c77f4ccfe14ba45f283a34ac9e85498a6b12
Divyatmika S <divyatmika@Divyatmikas-MacBook-Pro.local>
2020-06-10 15:26:32 -0700

    Updates

---
+++ precache-manifest.4a53e23d2a96c979b2981e342d6c8fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb36db9b595975129075c1ee9b1dc5d",
+    "url": "/meme-gen-react-app/index.html"
+  },
+  {
+    "revision": "b9cf58b0e3ea8c69419a",
+    "url": "/meme-gen-react-app/static/css/main.2107b84c.chunk.css"
+  },
+  {
+    "revision": "0248db98a3b031dcd53b",
+    "url": "/meme-gen-react-app/static/js/2.c253978f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-gen-react-app/static/js/2.c253978f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cf58b0e3ea8c69419a",
+    "url": "/meme-gen-react-app/static/js/main.5afc5de1.chunk.js"
+  },
+  {
+    "revision": "7e69d1bbdfa51cfa971d",
+    "url": "/meme-gen-react-app/static/js/runtime-main.b219c61e.js"
+  }
+]);
